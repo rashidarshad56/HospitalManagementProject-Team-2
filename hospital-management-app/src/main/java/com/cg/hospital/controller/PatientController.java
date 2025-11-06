f9b4b50356f58dc347c919ac9cc2b6e8af3fd298
Jaya-Gayatri <vukkum.jaya-gayatri@capgemini.com>
2025-11-06 15:24:27 +0530

    commit
    PatientController and PrescribeController

---
+++ hospital-management-app/src/main/java/com/cg/hospital/controller/PatientController.java
@@ -0,0 +1,24 @@
+package com.cg.hospital.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.hospital.entity.Patient;
+import com.cg.hospital.service.PatientService;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping
+    public List<Patient> getAllPatients() {
+        return patientService.getAllPatients();
+    }
+}
