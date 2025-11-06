f2ad79c7fa7cf3df99442387691988c6dd596eb4
Jaya-Gayatri <vukkum.jaya-gayatri@capgemini.com>
2025-11-06 15:25:47 +0530

    commit
    PrescibesId Entity

---
+++ hospital-management-app/src/main/java/com/cg/hospital/entity/PrescribesId.java
@@ -0,0 +1,53 @@
+package com.cg.hospital.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class PrescribesId implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long patient;
+    private Integer medication;
+    private LocalDateTime date;
+
+    public Long getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Long patient) {
+        this.patient = patient;
+    }
+
+    public Integer getMedication() {
+        return medication;
+    }
+
+    public void setMedication(Integer medication) {
+        this.medication = medication;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PrescribesId)) return false;
+        PrescribesId that = (PrescribesId) o;
+        return Objects.equals(patient, that.patient) &&
+               Objects.equals(medication, that.medication) &&
+               Objects.equals(date, that.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(patient, medication, date);
+    }
+}
