74944486ed1683617958f1fcb5c9e13235cf7fc5
Ankitha-Sangani <SANGANI.ANKITHA@CAPGEMINI.COM>
2025-11-06 23:20:28 +0530

    commit
    StayService

---
+++ hospital-management-app/src/main/java/com/cg/hospital/service/StayService.java
@@ -0,0 +1,9 @@
+package com.cg.hospital.service;
+
+import com.cg.hospital.entity.Stay;
+import java.util.List;
+
+public interface StayService {
+    List<Stay> getAllStays();  
+    List<Stay> getStaysByPatientSsn(int ssn);
+}
