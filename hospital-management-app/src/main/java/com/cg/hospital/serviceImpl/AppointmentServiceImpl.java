e3e71efb12c9b8f896526e3f4d4adc53fc44d925
Nikhitha-Yerra11 <nikhitha-siva.durga-yerra@capgemini.com>
2025-11-07 09:17:49 +0530

    Add files via upload

---
+++ hospital-management-app/src/main/java/com/cg/hospital/serviceImpl/AppointmentServiceImpl.java
@@ -0,0 +1,35 @@
+package com.cg.hospital.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cg.hospital.dto.AppointmentPatientDTO;
+import com.cg.hospital.repositories.AppointmentRepository;
+import com.cg.hospital.service.AppointmentService;
+
+@Service
+public class AppointmentServiceImpl implements AppointmentService{
+
+	 @Autowired
+	    private AppointmentRepository appointmentRepository;
+
+
+    @Override
+    public List<AppointmentPatientDTO> getPatientsByPhysicianEmployeeId(int employeeId) {
+    	return appointmentRepository.findByPhysician_EmployeeId(employeeId)
+	             .stream()
+	             .map(a -> new AppointmentPatientDTO(
+	                     a.getAppointmentId(),
+	                     a.getPatient().getName(),
+	                     a.getPatient().getAddress(),
+	                     a.getPatient().getPhone(),
+	                     a.getStarto(),
+	                     a.getEndo()
+	             ))
+	             .toList();
+    	}
+}
+
+
