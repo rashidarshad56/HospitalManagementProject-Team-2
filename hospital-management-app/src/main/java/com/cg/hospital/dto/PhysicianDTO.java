75e327846cad6ab1a872cf870f0be8a3aa88f1de
rashidarshad56 <rashid.arshad@capgemini.com>
2025-11-07 10:10:24 +0530

    Add files via upload

--- hospital-management-app/src/main/java/com/cg/hospital/dto/PhysicianDTO.java
+++ hospital-management-app/src/main/java/com/cg/hospital/dto/PhysicianDTO.java
@@ -1,44 +1,19 @@
 package com.cg.hospital.dto;
 
 public class PhysicianDTO {
-	
-	private int employeeId;
-	private String name;
-	private String position;
-	private int ssn;
-	
-	public PhysicianDTO() {
-		
-	}
-	public PhysicianDTO(int employeeId, String name, String position, int ssn) {
-		super();
-		this.employeeId = employeeId;
-		this.name = name;
-		this.position = position;
-		this.ssn = ssn;
-	}
-	public int getEmployeeId() {
-		return employeeId;
-	}
-	public void setEmployeeId(int employeeId) {
-		this.employeeId = employeeId;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPosition() {
-		return position;
-	}
-	public void setPosition(String position) {
-		this.position = position;
-	}
-	public int getSsn() {
-		return ssn;
-	}
-	public void setSsn(int ssn) {
-		this.ssn = ssn;
-	}
+    private String name;
+    private String position;
+
+    public PhysicianDTO() {}
+
+    public PhysicianDTO(String name, String position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getPosition() { return position; }
+    public void setPosition(String position) { this.position = position; }
 }
