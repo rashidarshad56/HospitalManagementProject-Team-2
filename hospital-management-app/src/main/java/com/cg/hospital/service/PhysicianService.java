fb7e4357ae20764acbadc86199845e7b70740738
rashidarshad56 <rashid.arshad@capgemini.com>
2025-11-07 10:12:52 +0530

    Add files via upload

--- hospital-management-app/src/main/java/com/cg/hospital/service/PhysicianService.java
+++ hospital-management-app/src/main/java/com/cg/hospital/service/PhysicianService.java
@@ -1,7 +1,11 @@
 package com.cg.hospital.service;
 
 import com.cg.hospital.dto.DepartmentPhysicianResponseDTO;
+import com.cg.hospital.dto.PhysicianDTO;
+
+import java.util.List;
 
 public interface PhysicianService {
+    List<PhysicianDTO> getAllPhysicians();
     DepartmentPhysicianResponseDTO getPhysiciansByDepartment(Integer departmentId);
 }
