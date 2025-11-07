913705d9aabe61939c78594d8f67dd908949bc1e
Ankitha-Sangani <SANGANI.ANKITHA@CAPGEMINI.COM>
2025-11-07 14:38:06 +0530

    Delete hospital-management-app/src/main/java/com/cg/hospital/service/StayService.java

--- hospital-management-app/src/main/java/com/cg/hospital/service/StayService.java
+++
@@ -1,9 +0,0 @@
-package com.cg.hospital.service;
-
-import com.cg.hospital.entity.Stay;
-import java.util.List;
-
-public interface StayService {
-    List<Stay> getAllStays();  
-    List<Stay> getStaysByPatientSsn(int ssn);
-}
