437f680255c8a6863ba30300f31d53a738daf115
rashidarshad56 <rashid.arshad@capgemini.com>
2025-11-07 13:59:57 +0530

    Delete hospital-management-app/src/main/java/com/cg/hospital/service/PrescribeService.java

--- hospital-management-app/src/main/java/com/cg/hospital/service/PrescribeService.java
+++
@@ -1,8 +0,0 @@
-package com.cg.hospital.service;
-
-import java.util.List;
-import com.cg.hospital.dto.PrescriptionDTO;
-
-public interface PrescribeService {
-    List<PrescriptionDTO> getPrescriptionsByPatientId(Long patientId);
-}
