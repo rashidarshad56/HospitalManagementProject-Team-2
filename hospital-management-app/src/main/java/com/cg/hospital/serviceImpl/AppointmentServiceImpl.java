c54af16fe80232ec348e6f04cb64fa4bc5d6e976
Nikhitha-Yerra11 <nikhitha-siva.durga-yerra@capgemini.com>
2025-11-08 11:19:44 +0530

    Add files via upload

--- hospital-management-app/src/main/java/com/cg/hospital/serviceImpl/AppointmentServiceImpl.java
+++ hospital-management-app/src/main/java/com/cg/hospital/serviceImpl/AppointmentServiceImpl.java
@@ -1,4 +1,4 @@
-package com.cg.hospital.serviceImpl;
+package com.cg.hospital.service.impl;
 
 import java.util.List;
 
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.cg.hospital.dto.AppointmentPatientDTO;
 import com.cg.hospital.entity.Appointment;
-import com.cg.hospital.entity.Patient;
-import com.cg.hospital.repository.AppointmentRepository;
+import com.cg.hospital.exception.AppointmentNotFoundException;
+import com.cg.hospital.repositories.AppointmentRepository;
 import com.cg.hospital.service.AppointmentService;
 
 @Service
@@ -17,30 +17,27 @@ public class AppointmentServiceImpl implements AppointmentService{
 	 @Autowired
 	    private AppointmentRepository appointmentRepository;
 
+    @Override
+    public List<AppointmentPatientDTO> getPatientsByPhysicianEmployeeId(int employeeId) {
+    	List<Appointment> appointments = appointmentRepository.findByPhysician_EmployeeId(employeeId);
+    	if (appointments.isEmpty()) {
+            throw new AppointmentNotFoundException("No appointments found for physician");
+        }
 
-//    @Override
-//    public List<Patient> getPatientsByPhysicianEmployeeId(int employeeId) {
-//        return appointmentRepository.findByPhysician_EmployeeId(employeeId)
-//                                    .stream()
-//                                    .map(Appointment::getPatient)
-//                                    .distinct()
-//                                    .toList();
-//    }
-	 @Override
-	 public List<AppointmentPatientDTO> getPatientsByPhysicianEmployeeId(int employeeId) {
-	     return appointmentRepository.findByPhysician_EmployeeId(employeeId)
+    	return appointmentRepository.findByPhysician_EmployeeId(employeeId)
 	             .stream()
 	             .map(a -> new AppointmentPatientDTO(
+	            		 a.getPhysician().getName(),
 	                     a.getAppointmentId(),
 	                     a.getPatient().getName(),
 	                     a.getPatient().getAddress(),
 	                     a.getPatient().getPhone(),
 	                     a.getStarto(),
-	                     a.getEndo()
+	                     a.getEndo(),
+	                     a.getExaminationRoom()
 	             ))
 	             .toList();
-	 }
+    	}
 }
-
 
 
