908cdde63eb10b11590b70bf9abbec6f17f9beae
durgaphanindram <MULLAPUDI-DURGA.PHANINDRA@CAPGEMINI.COM>
2025-11-09 18:16:20 +0530

    Add files via upload

--- hospital-management-app/src/main/java/com/cg/hospital/repository/PhysicianRepository.java
+++ hospital-management-app/src/main/java/com/cg/hospital/repository/PhysicianRepository.java
@@ -1,23 +1,19 @@
 package com.cg.hospital.repository;
-
+ 
 import com.cg.hospital.dto.PhysicianDTO;
 import com.cg.hospital.entity.Physician;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
+ 
 import java.util.List;
-
+ 
 public interface PhysicianRepository extends JpaRepository<Physician, Integer> {
-
-    // Fetch all physicians (optional filtering)
-    @Query("SELECT new com.cg.hospital.dto.PhysicianDTO(p.name, p.position) FROM Physician p")
-    List<PhysicianDTO> findAllPhysicians();
-
+ 
     // Fetch physicians by department ID
     @Query("SELECT new com.cg.hospital.dto.PhysicianDTO(p.name, p.position) " +
            "FROM Physician p JOIN p.affiliations a WHERE a.department.departmentId = :deptId")
     List<PhysicianDTO> findPhysiciansByDepartmentId(@Param("deptId") Integer deptId);
     List<Physician> findByAffiliationsDepartmentDepartmentId(Integer departmentId);
-
+ 
 }
